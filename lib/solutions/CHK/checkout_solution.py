d7173c6e478ed38f89b05d8ddef3b8cd1767e06d
sbx_user1051 <sbx_user1051@169.254.50.213>
2023-11-17 21:37:31 +0000

    Fri Nov 17 21:37:31 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,28 +5,25 @@
 from collections import defaultdict
 prices = {"A":50,"B":30,"C":20,"D":15,"E":40}
 offers = {"A":[(5,200),(3,130)],"B":[(2,45)], "E":[(2,80)]}
-shoppingFreq = defaultdict(int)
 
-def applyDisc(x,y,res) -> None:
+def applyDisc(x,y,res,shoppingFreq) -> None:
     if x in shoppingFreq and shoppingFreq[x] >=2 and y in shoppingFreq:
         free = shoppingFreq[x] // 2
         if shoppingFreq[y] >= free:
             res -= free*prices[y]
         else:
             res -= shoppingFreq[y]* prices[y]
-            
 
 def checkout(skus: str) -> int:
     res = 0
-    if type(skus) is not str:
-        return -1
-    
     n = len(skus)
-    
+    shoppingFreq = defaultdict(int)
+
     for x in range(n):
-        if not skus[x] in prices:
+        if skus[x] in prices:
+            shoppingFreq[skus[x]]+=1
+        else:
             return -1
-        shoppingFreq[skus[x]]+=1
     
     # iterate through all the items
     for item, freq in shoppingFreq.items():
@@ -38,7 +35,9 @@ def checkout(skus: str) -> int:
                     freq -= count
         res += freq * prices[item]
         
-        applyDisc('E','B',res)
+        
+        applyDisc('E','B',res,shoppingFreq)
                 
     return res
+
 
