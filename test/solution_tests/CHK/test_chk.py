d1b4709c111037dfe4505b2f9e5a8d1f7c46620f
sbx_user1051 <sbx_user1051@169.254.50.213>
2023-11-17 20:17:29 +0000

    Fri Nov 17 20:17:29 UTC 2023

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,7 +1,17 @@
 from lib.solutions.CHK import checkout_solution
 
 class TestSum():
-    def test_sum(self):
+    def test_basic(self):
         assert checkout_solution.checkout("AB") == 80
+    
+    def test_offer(self):
+        assert checkout_solution.checkout("AAA") == 150
+    
+    def test_offer_incomplete(self):
+        assert checkout_solution.checkout("AAAA") == 200
+    
+    def test_large(self):
+        assert checkout_solution.checkout("AAAAAAAAAAAABBBBCD") == 725
         
+
 
