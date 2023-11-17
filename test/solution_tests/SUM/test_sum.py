e3fde3bd38a1f771e67916b38303457621619cc1
sbx_user1051 <sbx_user1051@169.254.50.213>
2023-11-17 19:49:13 +0000

    Fri Nov 17 19:49:13 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,17 @@
-from SUM import sum_solution
+from lib.solutions.SUM import sum_solution
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+        
+    def test_sumTwo(self):
+        assert sum_solution.compute(1,43) == 44
+        
+    def test_sumThree(self):
+        assert sum_solution.compute(-11,43) == 32
+    
+    def test_sumFour(self):
+        assert sum_solution.compute(-11,-20) == -31
+        
+
 
--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -5,7 +5,6 @@ class TestSum():
     def test_sum(self):
         assert one.get() == 1
     
-    def test_sumTwo(self):
-        assert sum_solution.compute(1,43) == 44
+
 
 
