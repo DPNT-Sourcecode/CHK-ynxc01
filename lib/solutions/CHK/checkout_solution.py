98dec5f686705b5cd7c8af0b724a367ede09e6d6
sbx_user1051 <sbx_user1051@169.254.50.213>
2023-11-17 21:31:30 +0000

    Fri Nov 17 21:31:30 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,56 +4,41 @@
 # skus = unicode string
 from collections import defaultdict
 prices = {"A":50,"B":30,"C":20,"D":15,"E":40}
-offers = {"A":{5:200,3:130},"B":{2:45}}
-free_items = {"E":(2,"B",1)}
+offers = {"A":[(5,200),(3,130)],"B":[(2,45)], "E":[(2,80)]}
+shoppingFreq = defaultdict(int)
+
+def applyDisc(x,y,res) -> None:
+    if x in shoppingFreq and shoppingFreq[x] >=2 and y in shoppingFreq:
+        free = shoppingFreq[x] // 2
+        if shoppingFreq[y] >= free:
+            res -= free*prices[y]
+        else:
+            res -= shoppingFreq[y]* prices[y]
+            
+
 def checkout(skus: str) -> int:
+    res = 0
     if type(skus) is not str:
         return -1
     
-    shoppingFreq = defaultdict(int)
-    res = 0
     n = len(skus)
+    
     for x in range(n):
         if not skus[x] in prices:
             return -1
         shoppingFreq[skus[x]]+=1
     
     # iterate through all the items
-    for item in shoppingFreq.keys():
+    for item, freq in shoppingFreq.items():
         
-        freq = shoppingFreq[item]
-        # if the item is in the offer check if it's fulfilled
-        if not item in offers:
-            res += prices[item]*freq
-        else:
-            deal = offers[item]
-            # calculate how many offers can be fulfilled
-            keys = list(deal.keys())
-            if len(deal) > 1:
-                keys.sort()
-                rp = len(keys)-1
-                flag = True
-                
-                while freq > 0 and rp >= 0:
-                    if freq >= keys[rp]:
-                        freq -= keys[rp]
-                        # calc the fulfillment
-                        res += deal[keys[rp]]
-                    else:
-                        rp-=1
-                
-                if freq > 0:
-                    res += prices[item] * freq
-            else:
-                totalOfferFulfilled = freq// keys[0]
-                totalStockLeft = freq % keys[0]
-                
-                res+= totalOfferFulfilled*deal[keys[0]]
-                res+= totalStockLeft*prices[item]
-            
-        # if item in free_items:
-        #     q, other, q2 = free_items[item]
-        #     totalOfferFulfilled = freq// q
-        #     res += totalOfferFulfilled * (prices[other]*q2)
+        if item in offers:
+            for count, price in sorted(offers[item], reverse=True):
+                while freq >= count:
+                    res += price
+                    freq -= count
+        res += freq * prices[item]
         
+        applyDisc('E','B',res)
+                
     return res
+
