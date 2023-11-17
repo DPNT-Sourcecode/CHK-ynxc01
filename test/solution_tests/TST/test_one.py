94fd51107974f139d07569aa0b4112082e866d40
sbx_user1051 <sbx_user1051@169.254.50.213>
2023-11-17 19:43:14 +0000

    Fri Nov 17 19:43:14 UTC 2023

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -1,10 +1,11 @@
-from solutions.TST import one
-from solutions.SUM import compute
+from lib.solutions.TST import one
+from lib.solutions.SUM import sum_solution
 
 class TestSum():
     def test_sum(self):
         assert one.get() == 1
     
     def test_sumTwo(self):
-        assert compute(1,43) == 44
+        assert sum_solution.compute(1,43) == 44
+
 
