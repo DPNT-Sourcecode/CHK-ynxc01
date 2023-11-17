6133941dff6f62cd3f0ae79cfb8623763d426216
sbx_user1051 <sbx_user1051@169.254.50.213>
2023-11-17 21:59:49 +0000

    Fri Nov 17 21:59:49 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,21 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 from collections import defaultdict
-prices = {"A":50,"B":30,"C":20,"D":15,"E":40,"F":10}
-offers = {"A":[(5,200),(3,130)],"B":[(2,45)], "E":[(2,80)], "F":[(3,20)]}
-
-
+prices = {
+        'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10, 'I': 35,
+        'J': 60, 'K': 80, 'L': 90, 'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
+        'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90, 'Y': 10, 'Z': 50
+    }
+offers = {
+    'A': [(5, 200), (3, 130)],
+    'B': [(2, 45)],
+    'H': [(10, 80), (5, 45)],
+    'K': [(2, 150)],
+    'P': [(5, 200)],
+    'Q': [(3, 80)],
+    'V': [(3, 130), (2, 90)],
+    'U': [(3, 80)],
+}
 
 def checkout(skus: str) -> int:
     res = 0
@@ -22,6 +33,14 @@ def checkout(skus: str) -> int:
     free = shoppingFreq.get('E',0)//2
     if 'B' in shoppingFreq:
         shoppingFreq['B'] = max(0, shoppingFreq['B'] - free)
+        
+    free = shoppingFreq.get('N',0)//2
+    if 'M' in shoppingFreq:
+        shoppingFreq['M'] = max(0, shoppingFreq['B'] - free)
+    
+    free = shoppingFreq.get('R',0)//2
+    if 'Q' in shoppingFreq:
+        shoppingFreq['Q'] = max(0, shoppingFreq['B'] - free)
     
     # iterate through all the items
     for item, freq in shoppingFreq.items():
@@ -35,6 +54,7 @@ def checkout(skus: str) -> int:
         
                 
     return res
+
 
 
 
