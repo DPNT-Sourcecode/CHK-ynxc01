ca1acf9fc8efa37e72021c7a4fb55e8a7eb9f04c
sbx_user1051 <sbx_user1051@169.254.50.213>
2023-11-17 20:03:53 +0000

    Fri Nov 17 20:03:53 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,7 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    if not type(skus,str):
+        return -1
+
